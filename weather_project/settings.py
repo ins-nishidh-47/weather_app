cc2a4c51d745af27ab8a58e109a004fec2348eaa
Nishidh-ins-47 <nishidhsingh97@gmail.com>
2025-03-31 15:19:37 +0530

    weather_app

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
-from pathlib import Path
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +77,7 @@ WSGI_APPLICATION = 'weather_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join("/tmp", "db.sqlite3"),  # Move database to writable directory
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
